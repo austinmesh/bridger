dd215b1e19567e501d4d33d292813a77a951a0e1
Andy Shinn <andys@andyshinn.as>
2024-06-26 16:57:48 -0500

    glob cannot be quoted

--- config/deploy.sh
+++ config/deploy.sh
@@ -6,6 +6,6 @@ QUADLET_LOCATION="config/quadlet"
 UNIT_LOCATION="$HOME/.config/containers/systemd"
 SERVICES=$(ls -1 "$QUADLET_LOCATION" | grep -E '\.container$' | sed 's/\.container$//')
 
-cp -r "$QUADLET_LOCATION/*" "$UNIT_LOCATION"
+cp -r $QUADLET_LOCATION/* "$UNIT_LOCATION"
 systemctl --user daemon-reload
 systemctl --user reload-or-restart "${SERVICES[@]}"
