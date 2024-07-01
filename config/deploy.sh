1efeafccde1ba0dfe5653f8303811465c211bc17
Andy Shinn <andys@andyshinn.as>
2024-06-30 21:27:38 -0500

    make sure we use the correct image

--- config/deploy.sh
+++ config/deploy.sh
@@ -6,6 +6,9 @@ QUADLET_LOCATION="config/quadlet"
 UNIT_LOCATION="$HOME/.config/containers/systemd"
 SERVICES=($(ls -1 "$QUADLET_LOCATION" | grep -E '\.container$' | sed 's/\.container$//'))
 
+export IMAGE="${IMAGE:-ghcr.io/austinmesh/bridger:lastest}"
+
 cp -r $QUADLET_LOCATION/* "$UNIT_LOCATION"
+envsubst < "$QUADLET_LOCATION/bridger.container" > "$UNIT_LOCATION/bridger.container"
 systemctl --user daemon-reload
 systemctl --user reload-or-restart "${SERVICES[@]}"
