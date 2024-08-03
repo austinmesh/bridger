7275fb5b2fa72154742eb55f1893b40f8307e0d8
Andy Shinn <andys@andyshinn.as>
2024-08-02 23:44:53 -0500

    move to pyproject.toml

--- Containerfile
+++ Containerfile
@@ -1,6 +1,8 @@
 FROM docker.io/library/python:3.12
+ARG release
 WORKDIR /app
 COPY requirements.txt /app/requirements.txt
 RUN --mount=type=cache,target=/root/.pip pip install -r requirements.txt
 COPY . /app
+ENV SENTRY_RELEASE=$release
 CMD ["python", "-m", "bridge"]
--- bridge/__init__.py
+++ bridge/__init__.py
@@ -1,9 +1,6 @@
 import sentry_sdk
 from dotenv import load_dotenv
 
-__VERSION__ = "0.1.13"
-__APP_NAME__ = "bridger"
-
 load_dotenv()
 
 sentry_sdk.init(
@@ -11,5 +8,4 @@ sentry_sdk.init(
     send_default_pii=True,
     traces_sample_rate=1.0,
     profiles_sample_rate=1.0,
-    release=f"{__APP_NAME__}@{__VERSION__}",
 )
