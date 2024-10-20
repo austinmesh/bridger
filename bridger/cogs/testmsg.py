082e10bbb1571327e1267922fce3530af7891f60
Andy Shinn <andys@andyshinn.as>
2024-10-20 13:39:06 -0500

    WIP: traceroute packets

--- bridger/cogs/testmsg.py
+++ bridger/cogs/testmsg.py
@@ -1,33 +1,55 @@
 import os
-import aiomqtt
+import traceback
+from collections import deque
+from datetime import datetime
 from functools import partial
 
+import aiomqtt
+import pendulum
+from discord import Embed, Message
 from discord.ext import commands
+from meshtastic import protocols
+from meshtastic.protobuf.mesh_pb2 import RouteDiscovery, Routing
 from meshtastic.protobuf.mqtt_pb2 import ServiceEnvelope
-from meshtastic.protobuf.mesh_pb2 import Data
-from meshtastic.protobuf.portnums_pb2 import TEXT_MESSAGE_APP
-from google.protobuf.message import DecodeError
+from meshtastic.protobuf.portnums_pb2 import ROUTING_APP, TRACEROUTE_APP
 
-from bridger.__main__ import MQTT_USER, MQTT_PASS, MQTT_BROKER, MQTT_PORT
-from bridger.mqtt import MQTT_TOPIC
-from bridger.log import logger
+from bridger.__main__ import MQTT_BROKER, MQTT_PASS, MQTT_PORT, MQTT_USER
 from bridger.crypto import CryptoEngine
+from bridger.log import logger
+from bridger.mqtt import MQTT_TOPIC, PBPacketProcessor
 
-MQTT_TEST_CHANNEL_MESHTASTIC = os.getenv("MQTT_TEST_CHANNEL", "testing")
+MQTT_TEST_CHANNEL_MESHTASTIC = os.getenv("MQTT_TEST_CHANNEL", "LongFast")
 MQTT_TEST_CHANNEL_DISCORD = int(os.getenv("MQTT_TEST_CHANNEL_ID", 1253788609316913265))
 
 
 class TestMsg(commands.GroupCog, name="testmsg"):
-    delete_after = None
-
-    def __init__(self, bot: commands.Bot, discord_channel=None):
+    def __init__(self, bot: commands.Bot, discord_channel_id: int):
         self.bot = bot
-        self.discord_channel = discord_channel
+        self.discord_channel_id = discord_channel_id
+        self.discord_channel = None
 
     @commands.Cog.listener(name="on_ready")
     async def on_ready(self):
-        self.discord_channel = self.bot.get_channel(MQTT_TEST_CHANNEL_DISCORD)
+        # We have to do this here because the bot can't find the channel when done in setup()
+        self.discord_channel = self.bot.get_channel(self.discord_channel_id)
         logger.info(f"TestMsg cog is ready and channel is: {self.discord_channel}")
+
+    @staticmethod
+    def create_embed(service_envelope: ServiceEnvelope):
+        gateway = service_envelope.gateway_id
+        color = int(gateway[-6:], 16)
+        snr = service_envelope.packet.rx_snr
+        rssi = service_envelope.packet.rx_rssi
+        unix_time = service_envelope.packet.rx_time
+
+        formatted_time = datetime.fromtimestamp(unix_time).strftime("%Y-%m-%d %H:%M:%S")
+
+        embed = Embed(color=color)
+        embed.set_author(name=gateway)
+        embed.add_field(name="SNR", value=snr, inline=True)
+        embed.add_field(name="RSSI", value=rssi, inline=True)
+        embed.add_field(name="Time", value=formatted_time, inline=True)
+        return embed
 
     async def run_mqtt(self):
         topic = MQTT_TOPIC.removesuffix("/#")
@@ -35,6 +57,7 @@ class TestMsg(commands.GroupCog, name="testmsg"):
         full_topic = f"{topic}/{channel}/#"
 
         crypto = CryptoEngine()
+        queue = deque(maxlen=100)
 
         async with aiomqtt.Client(MQTT_BROKER, MQTT_PORT, username=MQTT_USER, password=MQTT_PASS) as client:
             await client.subscribe(full_topic)
@@ -45,21 +68,28 @@ class TestMsg(commands.GroupCog, name="testmsg"):
                 service_envelope = ServiceEnvelope.FromString(message.payload)
 
                 if service_envelope.packet.HasField("encrypted"):
-                    decrypted_data = crypto.decrypt(
-                        getattr(service_envelope.packet, "from"),
-                        service_envelope.packet.id,
-                        service_envelope.packet.encrypted
-                    )
+                    PBPacketProcessor.process_encrypted_data(service_envelope, crypto)
+                    app = ROUTING_APP
 
-                    try:
-                        data = Data()
-                        data.ParseFromString(decrypted_data)
-                        service_envelope.packet.decoded.CopyFrom(data)
-                    except DecodeError as e:
-                        logger.exception(f"Error decrypting message: {e}")
+                    if service_envelope.packet.decoded.portnum == app:
+                        packet_id = service_envelope.packet.id
+                        # payload = RouteDiscovery.FromString(service_envelope.packet.decoded.payload)
+                        # payload = Routing.FromString(service_envelope.packet.decoded.payload)
+                        payload = protocols[app].protobufFactory.FromString(service_envelope.packet.decoded.payload)
+                        source_node_id = getattr(service_envelope.packet, "from")
 
-                    if service_envelope.packet.decoded.portnum == TEXT_MESSAGE_APP:
-                        await self.discord_channel.send(service_envelope.packet.decoded.payload)
+                        if any(item[0] == packet_id for item in queue):
+                            message_id = next(item[1] for item in queue if item[0] == packet_id)
+                            message = await self.discord_channel.fetch_message(message_id)
+                            embeds = message.embeds
+                            embeds.append(self.create_embed(service_envelope))
+                            await message.edit(embeds=embeds)
+
+                        else:
+                            initial_embeds = [self.create_embed(service_envelope)]
+                            content = f"Traceroute received from **{source_node_id}**:\n```\n{payload}\n```"  # noqa: E501
+                            message: Message = await self.discord_channel.send(content, embeds=initial_embeds)
+                            queue.append((service_envelope.packet.id, message.id))
 
 
 def restart_mqtt_on_exception(task, bot: commands.Bot):
@@ -68,6 +98,7 @@ def restart_mqtt_on_exception(task, bot: commands.Bot):
         task.result()
     except Exception as e:
         print(f"Task failed with exception: {e}. Restarting task...")
+        traceback.print_exc()
         # Restart the task by creating a new task
         new_task = bot.loop.create_task(bot.cogs["testmsg"].run_mqtt())
         # Add the callback again to handle future exceptions
@@ -75,7 +106,7 @@ def restart_mqtt_on_exception(task, bot: commands.Bot):
 
 
 async def setup(bot: commands.Bot):
-    await bot.add_cog(TestMsg(bot))
+    await bot.add_cog(TestMsg(bot, MQTT_TEST_CHANNEL_DISCORD))
     run_mqtt_task = bot.loop.create_task(bot.cogs["testmsg"].run_mqtt())
     # Restart the task if it crashes
     run_mqtt_task.add_done_callback(partial(restart_mqtt_on_exception, bot=bot))
