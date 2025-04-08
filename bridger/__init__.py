7f94c3225ad6356b3ef7ba8d00ce9b94c86ba72d
Andy Shinn <andys@andyshinn.as>
2025-04-07 20:22:32 -0500

    fix sentry sample rate deprecation and make configurable

--- bridger/__init__.py
+++ bridger/__init__.py
@@ -1,11 +1,12 @@
+import os
+
 import sentry_sdk
 from dotenv import load_dotenv
 
 load_dotenv()
 
 sentry_sdk.init(
-    enable_tracing=True,
     send_default_pii=True,
-    traces_sample_rate=1.0,
-    profiles_sample_rate=1.0,
+    traces_sample_rate=float(os.getenv("SENTRY_TRACES_SAMPLE_RATE", 0.1)),
+    profiles_sample_rate=float(os.getenv("SENTRY_PROFILES_SAMPLE_RATE", 0.1)),
 )
