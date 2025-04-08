98a3389a48f1b8244a8dc7879fdb284bbae40226
Andy Shinn <andys@andyshinn.as>
2025-04-07 20:24:51 -0500

    there is no force_decode for locla CLI testing since moving to handler model

--- bridger/mesh/handlers/position.py
+++ bridger/mesh/handlers/position.py
@@ -10,7 +10,7 @@ class PositionHandler(PacketHandler):
     portnum = PortNum.POSITION_APP
 
     def handle(self):
-        if ("latitude_i" in self.payload_dict and "longitude_i" in self.payload_dict) or self.force_decode:
+        if "latitude_i" in self.payload_dict and "longitude_i" in self.payload_dict:
             # The GPS time field ends up being used by InfluxDB as the record time so we need to rename it
             if self.base_data.get("time"):
                 self.base_data["gps_time"] = self.base_data.pop("time", None)
