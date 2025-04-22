38feddabd6188903a2e3027625406da024b567c4
Andy Shinn <andys@andyshinn.as>
2025-04-21 20:25:42 -0500

    fix local force_decode

--- bridger/mesh/base.py
+++ bridger/mesh/base.py
@@ -3,11 +3,12 @@ from typing import Union
 
 
 class PacketHandler(ABC):
-    def __init__(self, packet, payload_dict, base_data, strip_text: bool = True):
+    def __init__(self, packet, payload_dict, base_data, strip_text: bool = True, force_decode: bool = False):
         self.packet = packet
         self.payload_dict = payload_dict
         self.base_data = base_data
         self.strip_text = strip_text
+        self.force_decode = force_decode
 
     @abstractmethod
     def handle(self) -> Union[None, dict]:
